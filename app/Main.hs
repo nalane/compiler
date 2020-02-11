f51c4cb681d0f507f5305fbe6e9f6406519fa502
nalane <nathaniel.patrick.lane@gmail.com>
2020-02-11 14:07:27 -0700

    Cleaned up code

--- app/Main.hs
+++ app/Main.hs
@@ -3,11 +3,15 @@ module Main where
 import Lib
 import Data.List
 import System.Environment
+import System.IO
 
 main :: IO ()
 main = do
+    -- Get the input file and pass it to the parsing function
     [file] <- getArgs
     parseTree <- parseTokens file
+
+    -- If Left, error. If Right, success
     case parseTree of
-        Left e -> print e
+        Left e -> hPrint stderr e
         Right t -> putStrLn $ intercalate "\n" $ map show t
--- src/Scanner.hs
+++ src/Scanner.hs
@@ -21,6 +21,7 @@ operators = [ ":=", "+", "-", "*", "/", "=", "!=", "<=", ">="
             , "<", ">", "(", ")", ";", ","
             ]
 
+-- Define the language
 tokenDef =
     emptyDef { Token.commentStart = ""
              , Token.commentEnd = ""
@@ -33,28 +34,53 @@ tokenDef =
              , Token.reservedOpNames = operators
              , Token.caseSensitive = True
              }
-
 lexer = Token.makeTokenParser tokenDef
 
-keyword = foldr ((<|>) . (\ x -> try (Token.reserved lexer x >> return x))) (fail "Could not match any keyword") keywords
-operator = foldr ((<|>) . (\ x -> try (Token.reservedOp lexer x >> return x))) (fail "Could not match any operators") operators
+-- The reserved and reservedOp functions in Parsec do not return the matched
+-- token. Instead, they take the expected token as input and succeed or fail.
+-- Therefore, we must iterate through each token; if it succeeds, return that
+-- token. If all fail, then we error.
+keyword = foldr ((<|>) . parseReserved) (fail failMsg) keywords where
+    failMsg = "Could not match any keyword"
+    parseReserved x = try (Token.reserved lexer x >> return x)
+operator = foldr ((<|>) . parseOperator) (fail failMsg) operators where
+    failMsg = "Could not match any operators"
+    parseOperator x = try (Token.reservedOp lexer x >> return x)
+
+-- The identifier and natural functions created by Parsec from the language
+-- definition are adequate for identifiers and int literals, respectively.
 identifier = Token.identifier lexer
-intLiteral = Token.natural lexer
+intLiteral = show <$> Token.natural lexer
+
+-- The default float literal function provided by Parsec will produce an
+-- actual Double value, rather than a string of the token. This is problematic
+-- when trying to print, as, for eaxample, 0.001 will print as 1e-3. Therefore,
+-- we must write our own float-parsing function.
 floatLiteral = do
     first <- many digit
     char '.'
     second <- many1 digit
     return (first ++ "." ++ second)
+
+-- Similarly, strings will be parsed with escaped characters being substituted.
+-- Thus, for example, the string "\n" will become an actual newline. Therefore,
+-- we must also write our own string parsing function.
 stringLiteral = do
     char '"'
     x <- many $ noneOf "\""
     char '"'
     return ("\"" ++ x ++ "\"")
 
+-- As above, we cycle through each type of token. If a parser succeeds,
+-- we consider it a match. If all parsers fail, it is an error.
 tokenTypes = [KEYWORD, OPERATOR, IDENTIFIER, FLOATLITERAL, INTLITERAL, STRINGLITERAL]
-tokenParsers = [keyword, operator, identifier, floatLiteral, show <$> intLiteral, stringLiteral]
-tok = foldr (<|>) (fail "Could not identify next token") $ zipWith (\t p -> try (ParseToken t <$> p)) tokenTypes tokenParsers
+tokenParsers = [keyword, operator, identifier, floatLiteral, intLiteral, stringLiteral]
+tok = foldr (<|>) (fail "Could not identify next token") tokGens where
+    tokGens = zipWith (\t p -> try (ParseToken t <$> p)) tokenTypes tokenParsers
 
+-- Read the complete contents of the file, then pass it into the parser.
+-- At this stage, the parser just gives us a list of tokens until we 
+-- reach the end of the file.
 parseTokens :: String -> IO (Either ParseError [ParseTree])
 parseTokens filename = parse (do
     toks <- many tok
